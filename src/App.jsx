776fde5be3e1672a918ab62d6767ec82fce1ed16
Piyush Yadav <piyushyadav0191@gmail.com>
2022-09-17 21:59:15 +0530

    addedd

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import MovieDetails from './components/MovieDetails'
 import MovieList from './components/MovieList'
 import Error from './components/Error'
 import Header from "./components/Header"
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+if (mobile) {
+   alert("Please View in Large screen")         
+} else {
+   toast.success("Welcome")
+}  
   return (
+   <>
+   
     <div className="App">
       <Router>
     <Header />
@@ -18,6 +28,7 @@ const App = () => {
         </Routes>
       </Router>
     </div>
+    </>
   )
 }
 
